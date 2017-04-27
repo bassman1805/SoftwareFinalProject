8ad86436035cb8ed517fb5a4839da5a550309cd9
Joshua Hoskinson <johoskin@mines.edu>
2017-04-27 12:12:52 -0600

    GUI popup pair programming

--- src/GUI/GameDisplay.java
+++ src/GUI/GameDisplay.java
@@ -17,7 +17,28 @@ public class GameDisplay extends JFrame{
 	private JMenuBar menus;
 	private JTextField textBase10, lvlCounter;
 	private JButton submit;
+	private JFrame game;
 	
 	private NumberGen generator;
+	
+	GameDisplay() {
+		game = new JFrame();
+		game.setSize(300,500);
+		//game.add(menus);
+		//game.add(textBase10);
+		//game.add(lvlCounter);
+		//game.add(submit);
+		
+		
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		GameDisplay g = new GameDisplay();
+		g.setVisible(true);
+	}
 
 }
