03764f16f40d67f219f7f50ba3da724749c3ada9
Joshua Hoskinson <johoskin@mines.edu>
2017-04-28 14:24:00 -0600

    User input class - pair programming

--- src/GUI/GameDisplay.java
+++ src/GUI/GameDisplay.java
@@ -1,10 +1,14 @@
 package GUI;
 
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 
 import Game.NumberGen;
@@ -22,9 +26,13 @@ public class GameDisplay extends JFrame{
 	private NumberGen generator;
 	
 	GameDisplay() {
-		game = new JFrame();
-		game.setSize(300,500);
-		//game.add(menus);
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300,500);
+		menus = new JMenuBar();
+		setJMenuBar(menus);
+		menus.add(createFileMenu());
 		//game.add(textBase10);
 		//game.add(lvlCounter);
 		//game.add(submit);
@@ -36,6 +44,28 @@ public class GameDisplay extends JFrame{
 	
 	
 	
+	private JMenu createFileMenu() {
+		JMenu menu = new JMenu("File");
+		menu.add(createFileExitItem());
+		return menu;
+	}
+
+	private JMenuItem createFileExitItem() {
+		JMenuItem item = new JMenuItem("Exit");
+		class MenuItemListener implements ActionListener {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+				
+			}
+		}
+		item.addActionListener(new MenuItemListener());
+		return item;
+	}
+
+
+
+
 	public static void main(String[] args) {
 		GameDisplay g = new GameDisplay();
 		g.setVisible(true);
